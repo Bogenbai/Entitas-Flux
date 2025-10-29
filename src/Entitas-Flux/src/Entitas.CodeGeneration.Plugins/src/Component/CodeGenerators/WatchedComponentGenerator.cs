1ddb2f021781710572a0eae77a6b35e6870987dd
Bogdan Kurilo <bogdan@estotyvilnius.com>
2025-10-29 10:17:43 +0200

    Bugfix: Can't generate multi-context watched components

--- src/Entitas-Flux/src/Entitas.CodeGeneration.Plugins/src/Component/CodeGenerators/WatchedComponentGenerator.cs
+++ src/Entitas-Flux/src/Entitas.CodeGeneration.Plugins/src/Component/CodeGenerators/WatchedComponentGenerator.cs
@@ -7,41 +7,25 @@ namespace Entitas.CodeGeneration.Plugins
 	public class WatchedComponentGenerator : AbstractGenerator
 	{
 		public override string Name => "Component (Watch Changes)";
-		
-		const string CHANGED_FLAG_TEMPLATE =
+
+		const string TEMPLATE =
 			@"[Entitas.CodeGeneration.Attributes.DontGenerate(false)]
 public class ${ComponentName}Changed : Entitas.IComponent { }
 ";
-		
+
 		public override CodeGenFile[] Generate(CodeGeneratorData[] data) => data
 			.OfType<ComponentData>()
-			.Where(d => d.ShouldGenerateMethods())
 			.Where(d => d.ShouldWatchChanges())
-			.SelectMany(Generate)
+			.Select(GenerateSingle)
 			.ToArray();
 
-		CodeGenFile[] Generate(ComponentData data) => data
-			.GetContextNames()
-			.Select(contextName => Generate(contextName, data))
-			.ToArray();
-		
-		CodeGenFile Generate(string contextName, ComponentData data)
+		CodeGenFile GenerateSingle(ComponentData data)
 		{
-			string template = CHANGED_FLAG_TEMPLATE;
+			var fileContent = TEMPLATE.Replace("${ComponentName}", data.ComponentName());
+			var fileName = (data.ComponentName() + "Changed").AddComponentSuffix();
+			var path = "Components" + Path.DirectorySeparatorChar + fileName + ".cs";
 
-			var fileContent = template
-				.Replace(data, contextName);
-
-			string fileName = data.ComponentNameWithContext(contextName) + "Changed";
-			fileName = fileName.AddComponentSuffix();
-			
-			return new CodeGenFile(
-				contextName + Path.DirectorySeparatorChar +
-				"Components" + Path.DirectorySeparatorChar +
-				fileName + ".cs",
-				fileContent,
-				GetType().FullName
-			);
+			return new CodeGenFile(path, fileContent, GetType().FullName);
 		}
 	}
 }
